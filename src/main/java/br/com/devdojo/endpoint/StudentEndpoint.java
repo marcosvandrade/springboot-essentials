9bbc291ecfe43240207c996a3e6ffcc69577561c
marcosvandrade <vascorj2006@gmail.com>
2020-07-21 09:18:09 -0300

    finalCommit

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -1,9 +1,5 @@
 package br.com.devdojo.endpoint;
 
-import br.com.devdojo.error.ResourceNotFoundException;
-import br.com.devdojo.model.Student;
-import br.com.devdojo.repository.StudentRepository;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devdojo.error.ResourceNotFoundException;
+import br.com.devdojo.model.Student;
+import br.com.devdojo.repository.StudentRepository;
 
 /**
  * @author William Suane for DevDojo on 6/5/17.
